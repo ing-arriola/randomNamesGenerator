5de56b1cffb2e0e98228eaec644e915dcc284024
ing-arriola <ingeniero.arriola@gmail.com>
2020-02-02 11:45:14 -0600

    add a check function for requested number of names

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 const formUI=document.getElementById('generar-nombre')
 
-formUI.addEventListener('submit',loadName)
+formUI.addEventListener('submit',generateUrl)
 
-function loadName(e){
+function generateUrl(e){
     e.preventDefault()
     //variables for the UI elements from i gonna take data
     const origin=document.getElementById('origen')
@@ -23,6 +23,11 @@ function loadName(e){
     if(amount !== ''){
         url+=`amount=${amount}&`
     }
+    
+    doTheRequest(url)
+}
+
+function doTheRequest(url){
     //new instance of httpRequest
     const xhr=new XMLHttpRequest()
     //Set the object
@@ -34,26 +39,56 @@ function loadName(e){
             const result=document.getElementById('resultado')
             //Getting the data
             let answer=JSON.parse(this.responseText)
+            let state=checkNames(answer)
             //a header for results :)
             let headerOfResults=document.createElement('h3')
+            
             headerOfResults.innerHTML='Generated Names'
+            
             //If the result sectio has any child.. remove it all of them :)
             while(result.hasChildNodes()){
                 result.removeChild(result.firstChild)
             }
             //Adding the header :)
             result.appendChild(headerOfResults)
+            result.appendChild(state)
+            result.appendChild(document.createElement('br'))//This only for a good looking :)
             answer.forEach(element => {
-                let nombre=document.createElement('h6')
-                nombre.classList.add('lista')
+                if(element.name !== ''){
+                    let nombre=document.createElement('h6')
+                    nombre.classList.add('lista')
+                    nombre.innerHTML=`${element.name}`
+                    result.appendChild(nombre)
+                }
                 
-                nombre.innerHTML=`${element.name}`
-                console.log(nombre)
-                result.appendChild(nombre)
             });
             
         }
     }
 
     xhr.send()
+}
+
+//This function checks if the response array has the same number of names as the user aks for
+//If the requested number of names are equal to the generated names then... print the results 
+//and add a success message... otherwise show a error message
+function checkNames(jsonarray){
+    let cuenta=0
+    let message=document.createElement('div')
+    
+    jsonarray.forEach(element => {
+        if(element.name !== ''){
+            cuenta+=1
+        }
+        
+    })
+    console.log("array es: " + jsonarray.length + "y cuenta es:"+cuenta)
+    if(cuenta >0 && cuenta === jsonarray.length){
+        message.innerHTML='Success'
+        message.classList.add('success')
+    }else{
+        message.innerHTML='At this moment the API DB has not enogth data'
+        message.classList.add('danger')
+    }
+    return message
 }
